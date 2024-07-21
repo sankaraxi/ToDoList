ed8e6eaa2d07992e8d9f58726bcc830917cdb788
sankaraxi <sankarfrompalani@gmail.com>
2024-07-22 02:10:59 +0530

    created a simple to-do list

--- script.js
+++ script.js
@@ -15,6 +15,31 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = taskInputBox.value;
         taskContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
     }
-    taskInputBox.value = '';    
+    taskInputBox.value = ''; 
+    saveData();   
 }
+
+taskContainer.addEventListener('click', function(event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked'); // when clicked, the task will be marked as done
+        saveData();
+    } else if (event.target.tagName === 'SPAN') {
+        let li = event.target.parentElement;
+        taskContainer.removeChild(li); // when clicked, the task will be removed
+        saveData();
+    }
+},false); // false is used to make sure the event is not captured in the capturing phase
+
+
+function saveData() {
+    localStorage.setItem('taskContainerData', taskContainer.innerHTML); // save the task list
+}
+
+function loadData() {
+    taskContainer.innerHTML = localStorage.getItem('taskContainerData'); // load the task list
+}
+loadData();
